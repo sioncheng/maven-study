23d43122debc8d74a4b9a6c2966108d1c11495b5
sioncheng <chengsion@hotmail.com>
2012-12-02 16:32:36 +0800

    simple maven proejct

---
+++ ch-simple/simple/src/main/java/org/sonatype/mavenbook/simple/App.java
@@ -0,0 +1,13 @@
+package org.sonatype.mavenbook.simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
